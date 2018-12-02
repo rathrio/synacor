e5e4e9ce253d63cef84ef990a23500c588a3ac57
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-12-02 23:43:22 +0100

    Implement halt and noop
    ༼ᕗຈل͜ຈ༽ᕗ yeah ༼ᕗຈل͜ຈ༽ᕗ

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -1,3 +1,5 @@
+use std::process;
+
 fn main() {
     let buffer = include_bytes!("../../challenge.bin");
 
@@ -6,6 +8,11 @@ fn main() {
         let high_byte = le_pair[1];
 
         let number = ((low_byte as u16) << 0) | ((high_byte as u16) << 8);
-        println!("{}", number);
+
+        match number {
+            0 => process::exit(0),
+            21 => (),
+            _ => (),
+        }
     }
 }
