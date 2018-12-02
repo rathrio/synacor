d8ff06e45a48a03e8f54eb78c16d6960a5c26e10
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-12-03 00:54:59 +0100

    Collect code 2

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -1,16 +1,31 @@
 use std::process;
+use std::collections::HashMap;
 
 fn main() {
     let buffer = include_bytes!("../../challenge.bin");
+    let mut numbers = buffer
+        .chunks(2)
+        .map(|le_pair| {
+            let low_byte = le_pair[0];
+            let high_byte = le_pair[1];
 
-    for le_pair in buffer.chunks(2) {
-        let low_byte = le_pair[0];
-        let high_byte = le_pair[1];
+            ((low_byte as u16) << 0) | ((high_byte as u16) << 8)
+        });
 
-        let number = ((low_byte as u16) << 0) | ((high_byte as u16) << 8);
+    let mut registers: HashMap<u16, u16> = HashMap::new();
 
-        match number {
+    loop {
+        let n = numbers.next().unwrap();
+
+        match n {
+            // HALT
             0 => process::exit(0),
+            // OUT
+            19 => {
+                let a = numbers.next().unwrap();
+                print!("{}", a as u8 as char);
+            },
+            // NOOP
             21 => (),
             _ => (),
         }
